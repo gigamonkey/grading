8fbed5109865c076dcb02cd8734dacaed14f6c17
Peter Seibel <peter@gigamonkeys.com>
2025-09-06 12:07:29 -0700

    Convert markup-to-questions to pugsql.

--- markup-to-questions.py
+++ markup-to-questions.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
+import pugsql
 from markup_quiz import load_questions
 import sys
-import sqlite3
 
-INSERT_QUESTION = "INSERT INTO questions (assignment_id, num, label, kind, question) values (?, ?, ?, ?, ?)"
-INSERT_NORMALIZED_ANSWER = "INSERT INTO normalized_answers (assignment_id, num, raw_answer, answer) values (?, ?, ?, ?)"
+
+def store_questions(db, questions):
+    for q in questions:
+        with db.transaction():
+            db.add_question(**q)
+            if "choices" in q:
+                for c in q["choices"]:
+                    # For MCQs each choice is its own normalized answer
+                    db.add_normalized_answer(
+                        assignment_id=q["assignment_id"],
+                        num=q["num"],
+                        raw_answer=c,
+                        answer=c,
+                    )
+            else:
+                print(f"No choices in {q}")
+
 
 if __name__ == "__main__":
-
-    conn = sqlite3.connect('db.db')
-    cursor = conn.cursor()
-
     [assignment_id, filename] = sys.argv[1:]
 
-    for q in load_questions(assignment_id, filename):
-        cursor.execute(INSERT_QUESTION, (q['assignment_id'], q['num'], q['label'], q['kind'], q['question']))
-        if 'choices' in q:
-            for c in q['choices']:
-                # For MCQs the choice is its own normalized answer
-                cursor.execute(INSERT_NORMALIZED_ANSWER, (q['assignment_id'], q['num'], c, c))
-        else:
-            print(f"No choices in {q}")
+    db = pugsql.module("sql")
+    db.connect("sqlite:///db.db")
 
-    conn.commit()
+    store_questions(db, load_questions(assignment_id, filename))
