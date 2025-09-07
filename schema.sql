c5342f00591cdd6bc025cb759b3fa6673d6ab6a8
Peter Seibel <peter@gigamonkeys.com>
2025-09-06 17:13:14 -0700

    Some new views for question scores and grades.

--- schema.sql
+++ schema.sql
@@ -64,11 +64,60 @@ VALUES
   (0.20, 1),
   (0.0, 0);
 
+-- Roster of students
 DROP TABLE IF EXISTS roster;
 .import --csv '| cat ../roster.json | mlr --ijson --ocsv cat' roster
 
-DROP VIEW IF EXISTS scored_student_answers;
-CREATE VIEW scored_student_answers as
-SELECT * from student_answers
-JOIN normalized_answers using (assignment_id, question_number, raw_answer)
-JOIN scored_answers using (assignment_id, question_number, answer);
+-- Compute per question scores for each student. Question score is computed
+-- differently depending on the kind of question. Currently handled are choices,
+-- freeanswer, ad mchoices.
+drop view if exists question_scores;
+create view question_scores as
+with mchoices as (
+  select
+    assignment_id,
+    question_number,
+    sum(case when score > 0 then score else 0 end) high,
+    sum(case when score < 0 then score else 0 end) low
+  from questions
+  join scored_answers using (assignment_id, question_number)
+  where kind = 'mchoices'
+  group by assignment_id, question_number
+)
+select
+  assignment_id,
+  question_number,
+  user_id,
+  kind,
+  group_concat(raw_answer) answers,
+  case
+    when kind = 'freeanswer' then score
+    when kind = 'choices' then score
+    when kind = 'mchoices' then (sum(score) - low) / (high - low)
+  end score
+from questions
+join student_answers using (assignment_id, question_number)
+join normalized_answers using (assignment_id, question_number, raw_answer)
+join scored_answers using (assignment_id, question_number, answer)
+left join mchoices using (assignment_id, question_number)
+group by user_id, assignment_id, question_number
+order by assignment_id, question_number, user_id;
+
+drop view if exists grades;
+create view grades as
+with
+   num_questions as (select assignment_id, count(*) num_questions from questions group by assignment_id),
+   scores as (
+     select assignment_id, user_id, sum(score == 0) wrong, sum(coalesce(score, 0)) / num_questions score
+     from roster
+     join question_scores using (user_id)
+     join num_questions using (assignment_id)
+     group by assignment_id, user_id
+   )
+select assignment_id, user_id, period, sortable_name, wrong, score, max(coalesce(fps.grade, 0)) grade
+     from roster
+     left join scores using (user_id)
+     left join fps on score >= minimum
+     where period in (1, 2)
+     group by assignment_id, user_id
+     order by period, sortable_name
