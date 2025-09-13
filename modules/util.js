fbe5f11f8bb54b698f8074e67f0bcebee438c7ec
Peter Seibel <peter@gigamonkeys.com>
2025-09-13 14:42:23 -0700

    Copy old util.js from last year.

--- modules/util.js
+++ modules/util.js
@@ -1,21 +1,75 @@
-import { readFileSync } from 'node:fs';
+import { execSync } from 'child_process';
+import { promisify } from 'util';
+import { argv } from 'process';
+import { readFileSync } from 'fs';
 
-const mapValues = (o, fn) => Object.fromEntries(Object.entries(o).map(([k, v]) => [k, fn(v, k)]));
+const { fromEntries, entries, keys, values, groupBy } = Object;
 
-const loadJSON = (filename) => JSON.parse(readFileSync(filename));
-
-const dumpJSON = (data) => console.log(JSON.stringify(data, null, 2));
-
-const dumpTSV = (objs) => {
-  objs.forEach(obj => {
-    console.log(Object.values(obj).join('\t'));
-  });
-};
+const mapValues = (o, fn) => fromEntries(entries(o).map(([k, v]) => [k, fn(v, k)]));
 
 const sum = (nums) => nums.reduce((tot, n) => tot + n, 0);
+
+const minimum = (nums) => nums.reduce((min, n) => Math.min(n, min), Infinity);
+
+const maximum = (nums) => nums.reduce((max, n) => Math.max(n, max), -Infinity);
 
 const count = (xs, fn) => xs.reduce((c, x) => c + (fn(x) ? 1 : 0), 0);
 
 const average = (nums) => sum(nums) / nums.length;
 
-export { mapValues, loadJSON, dumpJSON, dumpTSV, sum, count, average };
+const numberOr = (n, value) => (Number.isNaN(n) ? value : n);
+
+const loadJSON = (filename) => JSON.parse(readFileSync(filename));
+
+const dumpJSON = (data) => console.log(JSON.stringify(data, null, 2));
+
+const readLines = (filename) => readFileSync(filename, { encoding: 'utf-8' }).split(/\n/);
+
+const dumpTSV = (objs) => {
+  objs.forEach(obj => {
+    console.log(values(obj).join('\t'));
+  });
+};
+
+const fps = (n) => {
+  return (
+    n >= 0.85 ? 4
+      : n >= 0.70 ? 3
+      : n >= 0.45 ? 2
+      : n >= 0.20 ? 1
+      : 0
+  );
+};
+
+const numbers = (a, b) => {
+  console.log(`in numbers with ${a} and ${b}`)
+  return ns.map(Number);
+};
+
+const exec = (command, cwd) => {
+  return execSync(command, { cwd, encoding: 'utf-8', stdio: [ 'pipe', 'pipe', 'ignore' ] });
+};
+
+export {
+  argv,
+  average,
+  count,
+  dumpJSON,
+  dumpTSV,
+  entries,
+  exec,
+  fps,
+  fromEntries,
+  groupBy,
+  keys,
+  loadJSON,
+  mapValues,
+  maximum,
+  minimum,
+  numberOr,
+  numbers,
+  readFileSync,
+  readLines,
+  sum,
+  values,
+};
