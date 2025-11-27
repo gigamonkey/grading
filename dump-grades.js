8140818246baa32f7a37b6573f95b20629a9ca7e
Peter Seibel <peter@gigamonkeys.com>
2025-11-27 09:05:57 -0800

    Make dump-grades writes files unless run in dry-run mode.

--- dump-grades.js
+++ dump-grades.js
@@ -17,27 +17,37 @@ new Command()
   .description('Dump grades in JSON for a given assignment id.')
   .argument('[assignment]', 'Assignment', '*')
   .option('-u, --user <user>', 'User id')
+  .option('-n, --dry-run', "Don't write to file.")
   .action((assignment, opts) => {
 
     const assignmentId = intOrStar(assignment);
     const { user: userId } = opts;
 
     let data;
+    let filename;
 
     if (assignmentId && userId) {
       data = db.gradesForUserAndAssignment({assignmentId, userId});
+      filename = `${userId}-${assignmentId}`;
     } else if (assignmentId) {
       data = db.gradesForAssignment({assignmentId});
+      filename = assignmentId;
     } else if (userId) {
       data = db.gradesForUser({userId});
+      filename = userId;
     } else {
       data = db.allGrades();
+      filename = 'all';
     }
 
     if (!data) {
       console.warn("No data!!!");
     } else {
-      dumpJSON(data);
+      if (opts.dryRun) {
+        dumpJSON(data);
+      } else {
+        writeFileSync(path.join('grades', `${filename}.json`), JSON.stringify(data, null, 2), 'utf-8');
+      }
     }
 
   })
