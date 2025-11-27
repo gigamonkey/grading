78b35f333f45521317df3dc1ce758e63f2c46b36
Peter Seibel <peter@gigamonkeys.com>
2025-11-27 09:08:33 -0800

    Remove unnedded print.

--- score-answers.py
+++ score-answers.py
@@ -24,7 +24,6 @@ if __name__ == "__main__":
 
     questions = db.question_numbers(assignment_id=assignment_id)
     for q in questions:
-        print(q)
         question_number = q['question_number']
         question = q['question']
         kind = q['kind']
