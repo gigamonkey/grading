316975395c5a48053e20e8557c08dda9c1d1147c
Peter Seibel <peter@gigamonkeys.com>
2025-11-27 09:10:59 -0800

    Tweaks to load.sql

--- load.sql
+++ load.sql
@@ -2,12 +2,10 @@ BEGIN TRANSACTION;
 
 delete from assignments;
 delete from assignment_weights;
---delete from graded_assignments;
 delete from roster;
 
 .mode csv
-.import assignment-titles.csv assignments
---.import graded-assignments.csv graded_assignments
+.import assignments.csv assignments
 .import assignment-weights.csv assignment_weights
 .import '| cat ../roster.json | mlr --ijson --ocsv cat' roster
 
