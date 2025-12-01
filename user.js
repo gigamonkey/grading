a7b0298d2cafc6b5c0e922ab0ea1cadf97e9afd1
Peter Seibel <peter@gigamonkeys.com>
2025-12-01 13:30:37 -0800

    Script for looking up user info.

---
+++ user.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import { DB } from 'pugsql';
+import { Command, Option } from 'commander';
+import { camelify } from './modules/util.js';
+
+const { keys, fromEntries } = Object;
+
+const db = new DB('db.db')
+  .addQueries('modules/pugly.sql')
+  .addQueries('modules/queries.sql');
+
+const dumpAll = (u) => console.log(u);
+
+const dumpOne = (field) => {
+  return (u) => console.log(u[field]);
+}
+
+const dumpFields = (keys) => {
+  return (u) => {
+    console.log(fromEntries(keys.map(k => [k, u[k]])));
+  };
+};
+
+const main = async (q, opts) => {
+  const fields = keys(opts);
+  const dump = fields.length === 0
+        ? dumpAll
+        : fields.length === 1
+        ? dumpOne(fields[0])
+        : dumpFields(fields);
+  db.findUser({q: `%${q.toUpperCase()}%`}).map(camelify).forEach(dump);
+};
+
+new Command()
+  .description('Find user based on name or Github handle.')
+  .argument('q', 'Query')
+  .option('-u, --user-id', 'Get user ID')
+  .option('-s, --sortable-name', 'Get sortable name')
+  .option('-n, --name', 'Get name')
+  .option('-g, --github', 'Get Github handle')
+  .action(main)
+  .parse();
