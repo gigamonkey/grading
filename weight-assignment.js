34884374e09bc58c7f45a3fd0351432c28dca0ab
Peter Seibel <peter@gigamonkeys.com>
2025-12-30 13:08:44 -0800

    Make weight-assignment create assignment in db if needed.

--- weight-assignment.js
+++ weight-assignment.js
@@ -1,33 +1,52 @@
 #!/usr/bin/env node
 
-
+import { env } from 'node:process';
+import { API } from './api.js';
 import { DB } from 'pugsql';
 import { statSync } from 'node:fs';
 import { basename, dirname, join } from 'node:path';
 import { Command } from 'commander';
 import glob from 'fast-glob';
 import { getSha, getTimestamp, numCorrect, scoreTest } from './modules/grading.js';
-import { dumpJSON, loadJSON } from './modules/util.js';
+import { camelify, dumpJSON, loadJSON } from './modules/util.js';
 
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-
-
 
 const db = new DB('db.db')
   .addQueries('modules/pugly.sql')
   .addQueries('modules/queries.sql');
 
+
+const ensureAssignment = async (assignmentId, opts) => {
+
+  const assignment = db.assignment({ assignmentId });
+
+  if (assignment) {
+    return camelify(assignment);
+  } else {
+    const api = new API(opts.server, opts.apiKey);
+    const assignment = camelify(await api.assignment(assignmentId));
+    if (assignment) {
+      db.ensureAssignment(assignment);
+      return assignment;
+    } else {
+      throw new Error("No assignment for id " + assignmentId);
+    }
+  }
+};
+
 new Command()
   .description('Add assignment weights for an assignment')
   .argument('<id>', 'Assignment id')
   .option('-n, --dry-run', "Don't write to database.")
+  .option('-s, --server <url>', 'Server URL', env.BHS_CS_SERVER)
+  .option('-k, --api-key <key>', 'API key', env.BHS_CS_API_KEY)
   .action(async (assignmentId, opts) => {
-
     const rl = readline.createInterface({ input, output });
-
+    const assignment = await ensureAssignment(assignmentId, opts);
     const standards = db.courseStandards({assignmentId});
-    const { course_id: courseId, title } = db.assignment({assignmentId});
+    const { courseId, title } = assignment;
 
     standards.forEach((s, i) => {
       console.log(`[${i + 1}] ${s}`);
@@ -48,34 +67,6 @@ new Command()
     const weight = Number(await rl.question(`Weight for ${title}: `));
 
     db.insertAssignmentWeight({assignmentId, standard, weight});
-
-
-      // if (!opts.dryRun) {
-      //   db.ensureAssignment({ assignmentId, courseId, title });
-      //   db.clearJavaUnitTest({assignmentId});
-      //   db.clearScoredQuestionAssignment({assignmentId});
-      //   db.insertScoredQuestionAssignment({ assignmentId, questions });
-      // }
-
-      // results.forEach((file) => {
-      //   const d = dirname(file);
-      //   const github = basename(d);
-      //   const timestamp = getTimestamp(d);
-      //   const sha = getSha(d);
-      //   try {
-      //     const results = statSync(file).size > 0 ? loadJSON(file) : [];
-      //     const correct = numCorrect(results);
-      //     const score = scoreTest(results, questions);
-      //     if (!opts.dryRun) {
-      //       db.insertJavaUnitTest({assignmentId, github, correct, score, timestamp, sha});
-      //     } else {
-      //       console.log({assignmentId, github, correct, score, timestamp, sha});
-      //     }
-      //   } catch (e) {
-      //     console.log(e);
-      //   }
-      // });
-    //});
     rl.close();
 
   })
