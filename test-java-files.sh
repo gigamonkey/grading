464830239ad0c3a2b1306e611f1655ba51aa7bdb
Peter Seibel <peter@gigamonkeys.com>
2026-02-13 10:52:14 -0800

    Limit memory in test-java-files to be like Jobe server.

--- test-java-files.sh
+++ test-java-files.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+#set -x
 
 JAR=~/hacks/bhs-cs/java/target/bhs-cs.jar
+#JAR=~/hacks/jobe/jars/bhs-cs.jar
 RUNNER=com.gigamonkeys.bhs.testing.TestRunner
 
 tester="com.gigamonkeys.bhs.assignments.$1"
@@ -12,7 +14,7 @@ for f; do
     echo -n "Testing $f ... "
     rm -f "$(dirname "$f")/broken.txt"
     if [[ ! -e "$(dirname "$f")/missing.txt" ]]; then
-        if java -cp "$JAR" "$RUNNER" "$tester" "$f" > "$(dirname "$f")/results.json" 2> /dev/null; then
+        if java -Xrs -Xss8m -Xmx200m -cp "$JAR" "$RUNNER" "$tester" "$f" > "$(dirname "$f")/results.json" 2> /dev/null; then
             echo "ok."
         else
             echo "broken."
